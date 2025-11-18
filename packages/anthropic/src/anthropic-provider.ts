4945072095e3aff084dd5a24c30edd752920c11d
Nikolas Lazaris <niko@flatfile.io>
2025-11-17 22:57:33 -0500

    add supported urls for pdfs

--- packages/anthropic/src/anthropic-provider.ts
+++ packages/anthropic/src/anthropic-provider.ts
@@ -95,6 +95,7 @@ export function createAnthropic(
       generateId: options.generateId ?? generateId,
       supportedUrls: () => ({
         'image/*': [/^https?:\/\/.*$/],
+        'application/pdf': [/^https?:\/\/.*$/],
       }),
     });
 
